0d0e912bc760ac76d88c98221a4317879dcff136
Alexander <mypost@example.com>
2023-01-29 10:54:02 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿
 // task 58. Программа нахождения произведения двух матриц
 
-int[,] Matrix1 = new int [,]{{2, 4}, 
-                             {3, 2}};
-int[,] Matrix2 = new int [,]{{3, 4},
-                             {3, 3}};
+int[,] Matrix1 = new int [,]{{7, 0}, 
+                             {6, 8}};
+int[,] Matrix2 = new int [,]{{1, 1},
+                             {8, 4}};
 int[,] ResultMatrix = new int [Matrix1.GetLength(0), Matrix1.GetLength(0)];
 
 for (int i = 0; i < ResultMatrix.GetLength(0); i++)
